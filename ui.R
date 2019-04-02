fb9c0cafcd95282a90503e726f9bd0847442aa31
Anastasia1706 <36297148+Anastasia1706@users.noreply.github.com>
2019-04-02 19:31:01 +0300

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,40 @@
+library('data.table')
+library('shiny')
+library('dplyr')
+library('bindrcpp')
+shinyUI(
+  pageWithSidebar(
+    
+    # название приложения:
+    headerPanel('Статистика импорта'),
+    
+    # боковая панель
+    sidebarPanel(
+      # радиокнопки: период агрегирования данных
+      radioButtons('period.name', 
+                   'Выберите период агрегирования', 
+                   c('Месяц', 'Квартал'), 
+                   selected = 'Месяц'),
+      # слайдер: фильтр по годам
+      sliderInput('year.range', 'Месяцы:',
+                  min = 1, max = 12, value = c(1, 12),
+                  width = "100%", sep = ''),
+      # выпадающее меню: страна для отбора наблюдений
+      uiOutput('stateList'),
+      uiOutput('flow')
+
+    ),
+    
+    # главная область
+    mainPanel(
+      # текст с названием выбранной страны
+      textOutput('text'),
+      # график ряда
+      plotOutput('ts.plot'),
+      # таблица данных
+      dataTableOutput('table'),
+      # кнопка сохранения данных
+      actionButton('save.csv', 'Сохранить данные в .csv')
+    )
+  )
+)
